de217a909530a3d126c76f0cd9272f11083709a9
Besik  Rurua <brurua@Besiks-MacBook-Air.local>
2020-08-04 20:14:36 -0400

    new panel with STD, Min and Max, and with mouse hover Data Display

--- plugin/consumer-message-rate/src/SimplePanel.tsx
+++ plugin/consumer-message-rate/src/SimplePanel.tsx
@@ -4,6 +4,8 @@ import { SimpleOptions } from 'types';
 import { css, cx } from 'emotion';
 import { stylesFactory, useTheme } from '@grafana/ui';
 
+import * as stats from 'stats-lite';
+
 // what do we need?
 // 1. we need the messaging rate per consumer
 // 2. we need the highest message rate per (per 30 seconds)
@@ -16,25 +18,37 @@ interface MessagingRateQuarters {
   topMid: number;
   bottomMid: number;
   bottom: number;
+  stdev: number;
 }
 
 // takes in an array of consumer offsets, spaced at 30 seconds apart
 const getMessagingRate = (array: number[]): number[] => {
   const rateResponse: number[] = [1];
   for (let i = 1; i < array.length; i++) {
-    rateResponse[i] = array[i] - array[i - 1];
+    rateResponse[i] = array[i] - array[i - 1]; // 10 - 300 = 241   // [555, 990, 1000]
   }
   return rateResponse;
 };
 
 const getQuarters = (array: number[]): MessagingRateQuarters => {
-  array = array.sort().filter(x => x > 1);
-  const quarterLength = Math.floor(array.length / 4);
+  array = array.sort((a, b) => a - b).filter(x => x > 1);
+
+  let bottomMid;
+  let topMid;
+  if (array.length % 2 !== 0) {
+    topMid = Math.floor(array[array.length / 2 + 1]);
+    bottomMid = Math.floor(array[array.length / 2]);
+  } else {
+    topMid = array[array.length / 2];
+    bottomMid = array[array.length / 2 - 1];
+  }
+
   const response: MessagingRateQuarters = {
     top: array[array.length - 1] || 0,
-    topMid: array[quarterLength * 3] || 0,
-    bottomMid: array[quarterLength * 2] || 0,
-    bottom: array[quarterLength] || 0,
+    topMid: topMid || 0,
+    bottomMid: bottomMid || 0,
+    bottom: array[0] || 0,
+    stdev: Math.floor(stats.stdev(array)),
   };
   return response;
 };
@@ -49,9 +63,10 @@ interface Props extends PanelProps<SimpleOptions> {}
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme();
   const styles = getStyles();
-  console.log(data);
+  // console.log(data.series);
   let xOffset = 0;
   let maxHeight = 0;
+
   return (
     <div
       className={cx(
@@ -62,7 +77,21 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
         `
       )}
     >
-      <svg height={height} width={width}>
+      <svg
+        height={height}
+        width={width}
+        className={css`
+          g text {
+            display: none;
+          }
+          ,
+          g:hover text {
+            display: block;
+            font-size: 25px;
+            font-weight: bold;
+          }
+        `}
+      >
         {data.series
           .map(series =>
             getQuarters(getMessagingRate(series.fields[1].values.toArray().filter(x => x < 30000)).reverse())
@@ -72,15 +101,30 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
             return group;
           })
           .map((consumergroup, i) => {
-            xOffset += Math.floor(width / data.series.length);
+            xOffset = Math.floor(width / data.series.length) * i;
             return (
               <g>
+                <defs>
+                  <filter id="f1" x="0" y="0" width="200%" height="200%">
+                    <feOffset result="offOut" in="SourceGraphic" dx="2" dy="5" />
+                    <feGaussianBlur result="blurOut" in="offOut" stdDeviation="10" />
+                    <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+                  </filter>
+                </defs>
+                {/* <defs>
+                  <filter id="f1" x="0" y="0">
+                    <feGaussianBlur in="SourceGraphic" stdDeviation="15" />
+                  </filter>
+                </defs> */}
                 <rect
                   x={xOffset}
                   y={height * (consumergroup.topMid / maxHeight)}
                   height={height * (consumergroup.top / maxHeight) - height * (consumergroup.topMid / maxHeight)}
                   width={Math.floor((width / data.series.length) * 0.8)}
                   fill={getColor(i, data.series.length)}
+                  stroke="grey"
+                  stroke-width="3"
+                  filter="url(#f1)"
                 />
                 <rect
                   x={xOffset}
@@ -88,6 +132,9 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
                   height={height * (consumergroup.topMid / maxHeight) - height * (consumergroup.bottomMid / maxHeight)}
                   width={Math.floor((width / data.series.length) * 0.8)}
                   fill={getColor(i + 0.25, data.series.length)}
+                  stroke="grey"
+                  stroke-width="3"
+                  filter="url(#f1)"
                 />
                 <rect
                   x={xOffset}
@@ -95,6 +142,9 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
                   height={height * (consumergroup.bottomMid / maxHeight) - height * (consumergroup.bottom / maxHeight)}
                   width={Math.floor((width / data.series.length) * 0.8)}
                   fill={getColor(i + 0.5, data.series.length)}
+                  stroke="grey"
+                  stroke-width="3"
+                  filter="url(#f1)"
                 />
                 <rect
                   x={xOffset}
@@ -102,19 +152,29 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
                   height={height * (consumergroup.bottom / maxHeight)}
                   width={Math.floor((width / data.series.length) * 0.8)}
                   fill={getColor(i + 0.75, data.series.length)}
+                  stroke="grey"
+                  stroke-width="3"
+                  filter="url(#f1)"
                 />
-                <text x={xOffset} y={30}>
-                  {consumergroup.bottom}
-                </text>
-                <text x={xOffset} y={60}>
-                  {consumergroup.bottomMid}
-                </text>
-                <text x={xOffset} y={90}>
-                  {consumergroup.topMid}
-                </text>
-                <text x={xOffset} y={120}>
-                  {consumergroup.top}
-                </text>
+                <svg>
+                  <g>
+                    <text x={xOffset} y={230} fill="blue">
+                      {`BOTT ${consumergroup.bottom}`}
+                    </text>
+                    <text x={xOffset} y={260} fill="pink">
+                      {`BMD ${consumergroup.bottomMid}`}
+                    </text>
+                    <text x={xOffset} y={290} fill="yellow">
+                      {`TMD ${consumergroup.topMid}`}
+                    </text>
+                    <text x={xOffset} y={320} fill="white">
+                      {`TOP ${consumergroup.top}`}
+                    </text>
+                    <text x={xOffset} y={350} fill="red">
+                      {`STDEV ${consumergroup.stdev}`}
+                    </text>
+                  </g>
+                </svg>
               </g>
             );
           })}
